4c6b13190bf035f485ae54aee7139a9ea6d34d91
Mayelprince <princemayel86@gmail.com>
2024-08-22 13:57:02 +0100

    Add files via upload

---
+++ Commandes/repos.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/boniphace478/BONIPHACE-MD';
+  const img = 'https://telegra.ph/file/6f286ab70044f47140d2f.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `*hellow whatsaap user
+this is* *PRINCE-MD.*\n get session id *by*, *pairing code*  https://boniphace70-69db40dd600d.herokuapp.com/pair
+
+🗼 *REPOSITORY:* ${data.html_url}
+🌟 *STARS:* ${repoInfo.stars}
+🧧 *FORKS:* ${repoInfo.forks}
+📅 *RELEASE DATE:* ${releaseDate}
+🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+👨‍💻 *OWNER:* _PRINCE-MD_
+💞 *THEME:* _PRINCE-MD_
+
+__________________________________
+            *Made With PRINCE-MD*`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
